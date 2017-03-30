2a2cb8c62f87f51c0dce25118bc8a658f19a191e
jason chen <cjx89807@alibaba-inc.com>
2017-03-30 14:06:24 +0800

    chore: finish build scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,48 +1,13 @@
-const config = require('./config.json');
+// const config = require('./config.json');
+const util = require('./util');
 const cwd = process.cwd();
 const fs = require('fs');
 const path = require('path');
 
-const readmePath = path.resolve(cwd, 'README.md');
 const papersPath = path.resolve(cwd, 'papers');
+const paperFiles = fs.readdirSync(papersPath, 'utf8');
 
-const md = fs.readFileSync(readmePath, 'utf8').split('\n');
-const papers = fs.readdirSync(papersPath, 'utf8');
-
-function groupingByYear(samples) {
-    const dateReg = /(\d{4})-\d{1,2}-\d{1,2}/;
-    const result = [];
-    const preCache = {};
-    samples.forEach(function(it,id) {
-        const keys = it.match(dateReg);
-        if (keys && keys.length > 1) {
-            const year = keys[1];
-            if (year !== preCache.year) {
-                if (preCache.year) {
-                    result.push(Object.assign({}, preCache));
-                }
-                preCache.year = year;
-                preCache.papers = [];
-            }
-            preCache.papers.push(warpPaper(it));
-        }
-    })
-}
-function warpPaper(paper) {
-    const result = {};
-    let mark = ' ';
-    result.finished = checkStatus(paper);
-    result.paper = paper;
-    result.title = paper.replace(/(\d{4})-\d{1,2}-\d{1,2}|\.md/g, '');
-    result.url = config.prefix + paper;
-    mark = result.finished ? 'x' : ' ';
-    result.item = '- [' + mark + '] ' + result.url;
-    return result;
-}
-function checkStatus(paper) {
-    const result = false;
-    return result;
-}
 // const lines = md.split('\n');
-groupingByYear(papers);
-// console.log(papers);
+const papers = util.groupingByYear(paperFiles);
+util.updatedMD(papers);
+// console.log(bb);
---
+++ scripts/util.js
@@ -0,0 +1,87 @@
+const config = require('./config.json');
+const cwd = process.cwd();
+const fs = require('fs');
+const path = require('path');
+
+const readmePath = path.resolve(cwd, 'README.md');
+const md = fs.readFileSync(readmePath, 'utf8').split('\n');
+
+function groupingByYear(samples) {
+    const dateReg = /(\d{4})-\d{1,2}-\d{1,2}/;
+    const result = [];
+    const preCache = {};
+    samples.forEach(function (it, id) {
+        const keys = it.match(dateReg);
+        if (keys && keys.length > 1) {
+            const year = keys[1];
+            if (year !== preCache.year) {
+                if (preCache.year) {
+                    result.push(Object.assign({}, preCache));
+                }
+                preCache.year = year;
+                preCache.papers = [];
+            }
+            preCache.papers.push(warpPaper(it));
+        }
+    })
+    result.push(Object.assign({}, preCache));
+    return result;
+}
+
+function warpPaper(paper) {
+    const result = {};
+    let mark = ' ';
+    result.finished = checkStatus(paper);
+    result.paper = paper;
+    result.title = paper.replace(/(\d{4})-\d{1,2}-\d{1,2}-|\.md/g, '');
+    result.url = encodeURI(config.prefix + paper);
+    mark = result.finished ? 'x' : ' ';
+    result.item = `- [${mark}] [${result.title}](${result.url})`;
+    // console.log(result);
+    return result;
+}
+
+function checkStatus(paper) {
+    let result = false;
+    const reg = /\-\s\[(\s|x)]\s/;
+    md.forEach(function (it, id) {
+        if (reg.test(it) && it.indexOf(paper) > -1) {
+            const place = it.match(reg)[1];
+            result = place === 'x' ? true : false ;
+        }
+    })
+    return result;
+}
+
+function updatedMD(papers) {
+    const reg = /^### Papers of/;
+    let index = md.length;
+    for (let i = 0; i < index; i++) {
+        if (reg.test(md[i])) {
+            index = i;
+            break;
+        }
+    }
+    const result = md.slice(0, index);
+    let preYear = 0;
+    for (let j = 0; j < papers.length; j++) {
+        const paper = papers[j];
+        if (paper.year !== preYear) {
+            preYear = paper.year;
+            result.push('### Papers of ' + preYear);
+            result.push('');
+        }
+        paper.papers.forEach(function(it){
+            result.push(it.item);
+        })
+        // result.push();
+        result.push('');
+    }
+
+    // console.log(result);
+    // console.log(md);
+    fs.writeFileSync(readmePath, result.join('\n'));
+}
+
+exports.groupingByYear = groupingByYear;
+exports.updatedMD = updatedMD;
